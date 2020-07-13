439c256c256a55502cca4b5015932e15854ed3d5
stottbot <stott.jeffrey@gmail.com>
2020-07-13 11:20:55 -0400

    update

---
+++ HF_CreateBlankRecordingKeys.m
@@ -0,0 +1,38 @@
+function [] = HF_CreateBlankRecordingKeys(varargin)
+%% This function Generates the Keys File with the optional inputs left blank
+
+fd = FindFiles('*Events.nev');
+startSess = 1;
+endSess = length(fd);
+
+process_varargin(varargin);
+for iSess = startSess:endSess;
+    pushdir(fileparts(fd{iSess}));
+    
+    %%%%%%%%%%%%%%%%%%%
+    % generate keys.m %
+    %%%%%%%%%%%%%%%%%%%
+    SSN = HD_GetSSN;
+    disp(SSN);
+    
+    fout = cat(2,SSN,'_keys.m');
+    fout = regexprep(fout,'-','_');
+    fid = fopen(fout,'w');
+    %populate keys.m
+    fprintf(fid,'ExpKeys.Behavior = ''%s'';\n','HF Task');               % What is the task? Headfixed, freely moving, etc. 
+    fprintf(fid,'ExpKeys.Protocol = ''%s'';\n','Si Probe Recording');    % What kind of recordings are being done (probe type, or type of electrode)  
+    fprintf(fid,'ExpKeys.ProbeType = ''%s'';\n','A4x2tet');  
+    fprintf(fid,'ExpKeys.TimeOnTrack = %d;\n',NaN);   % if there is a pre-record and post-record period, this is the first timestamp of task onset. 
+    fprintf(fid,'ExpKeys.TimeOffTrack = %d;\n',NaN);   % if there is a pre-record and post-record period, this is the end of the task
+    fprintf(fid,'ExpKeys.VirusTarget1 = ''%s'';\n',''); % Name of brain area for virus injection 1, if applicable 
+    fprintf(fid,'ExpKeys.VirusTarget2 = ''%s'';\n',''); % Name of brain area for virus injection 2, if applicable
+    fprintf(fid,'ExpKeys.RecordingTarget = ''%s'';\n',''); % Which brain regions are being recorded from. 
+    fprintf(fid,'ExpKeys.TetrodeTargets = ''%s'';\n',''); % Where each tetrode is recording from (areas above).
+    fprintf(fid,'ExpKeys.DepthFromSkull = %d;\n',NaN); % Estimated depth from skull surface. Based on stereotax values.  
+    fprintf(fid,'ExpKeys.Notes = %d;\n', NaN);
+        
+    disp('keys file created')
+end
+
+
+
---
+++ plot_AHVtuning.m
@@ -0,0 +1,82 @@
+%% cd to data folder
+% miscellaney
+FontSize = 16;
+SSN = HD_GetSSN('SingleSession');
+%% get AHV
+cfg_AHV = [];
+cfg_AHV.subsample_factor = 10;
+[AHV_tsd, S, tc_out] = AHV_tuning(cfg_AHV);
+AHV_dt = median(diff(AHV_tsd.tvec));
+
+%% plot tuning curves
+fc = FindFiles('*.t', 'CheckSubdirs', 0);
+numCells = size(tc_out.tc, 1);
+clf
+
+for iCell = 1:numCells; % first row is Tuning Curves
+    [a, b, c] = fileparts(fc{iCell});
+    subplot(5,numCells,iCell)
+    set(gca, 'TickDir', 'out', 'FontSize', FontSize)
+    plot(tc_out.usr.binCenters, tc_out.tc(iCell,:), 'MarkerSize', 5);
+    xlabel('AHV (deg./sec)', 'FontSize', FontSize)
+    ylabel('FR (Hz)', 'FontSize', FontSize)
+    set(groot, 'DefaultLegendInterpreter', 'none')
+    title(strcat(SSN, '-', b), 'FontSize', FontSize)
+    
+end
+
+%% plot scatterplot
+cfg_Q = [];
+cfg_Q.smooth = 'gauss';
+cfg_Q.gausswin_sd = 0.05;
+cfg_Q.dt = AHV_dt;
+F = MakeQfromS(cfg_Q, S);
+
+% convert to FR
+F.data = F.data ./ cfg_Q.dt;
+
+% find FR corresponding to each AHV sample
+F_idx = nearest_idx3(AHV_tsd.tvec, F.tvec);
+AHV_F = F.data(:,F_idx);
+
+iCell = 0;
+for iPlot = numCells+1: 2*numCells;   % second row is Firing Rate scatterplot
+    iCell = iCell +1;
+    subplot(5,numCells,iPlot)
+    set(gca, 'TickDir', 'out', 'FontSize', FontSize)
+    plot(AHV_tsd.data, AHV_F(iCell,:), '.', 'MarkerSize', .5);
+    xlabel('AHV (deg./sec)', 'FontSize', FontSize)
+    ylabel('FR (Hz)', 'FontSize', FontSize)
+end
+
+%% acf
+cfg_acf = [];
+cfg_acf.binsize = 0.001;
+cfg_acf.max_t = 0.5;
+cfg_acf.smooth = 1; % set to 1 to compute ccf on SDF, 0 for raw spikes
+cfg_acf.gauss_w = 1; % width of Gaussian convolution window (in s)
+cfg_acf.gauss_sd = 0.005; % SD of Gaussian convolution window (in s)
+
+iCell = 0;
+for iPlot = 2*numCells+1: 3*numCells;   % third row is the autocorrelation
+    iCell = iCell +1;
+    subplot(5,numCells,iPlot)
+    set(gca, 'TickDir', 'out', 'FontSize', FontSize)
+    [acf, tvec] = ccf(cfg_acf, S.t{iCell}, S.t{iCell});
+    midpoint = ceil(length(acf)./2);
+    acf(midpoint) = 0;
+    plot(tvec, acf);
+    xlabel('Time (sec)', 'FontSize', FontSize)
+    set(gca, 'XTick', -0.5:0.1:0.5); grid on;
+end
+
+%% HistISI
+iCell = 0;
+for iPlot = 3*numCells+1: 4*numCells;   % fourth row is the HistISI
+    iCell = iCell +1;
+    subplot(5,numCells,iPlot)
+    set(gca, 'TickDir', 'out', 'FontSize', FontSize)
+    HistISIsubplot(S.t{iCell});
+    xlabel('Time (sec)', 'FontSize', FontSize)
+end
+
