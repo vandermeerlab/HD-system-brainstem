f9b61e981a3e52c6a93c5e06af7306bd39d34473
stottbot <stott.jeffrey@gmail.com>
2021-10-23 13:58:27 -0400

    added a parenthesis (1) after events_ts.t{StartIndex} in case there is more than one start time

--- plotting/plot_AHVtuningMegaplot3.m
+++ plotting/plot_AHVtuningMegaplot3.m
@@ -26,7 +26,7 @@ if subtractStartTime == 1 % New cheetah versions have timestamps
     wrapper = @(events_ts) strcmp(events_ts, 'Starting Recording');
     A = cellfun(wrapper, events_ts.label); 
     Startindex = find(A); % index which label says 'Start Recording' 
-    starttime = events_ts.t{Startindex};
+    starttime = events_ts.t{Startindex}(1); % use the very first start record time
     wrapper2 = @(events_ts) strcmp(events_ts, 'Stopping Recording');
     B = cellfun(wrapper2, events_ts.label);
     EndIndex = find(B); 
