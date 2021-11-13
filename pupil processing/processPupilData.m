3df51c7600b88fdda42f0243216461f79dd5d1df
stottbot <stott.jeffrey@gmail.com>
2021-11-13 14:19:29 -0500

    changed threshold names and removed some extraneous filetering, added more to plot

--- pupil processing/processPupilData.m
+++ pupil processing/processPupilData.m
@@ -1,4 +1,4 @@
-function [temporalSaccades, nasalSaccades, tsdH, tsdV, diffH, diffV, filteredH, filteredV] = processPupilData(cfg_in)
+function [temporalSaccades, nasalSaccades, tsdH, tsdV, diffH, diffV] = processPupilData(cfg_in)
 
 % JJS. 2021-03-13.
 % Basic working version that finds candidate saccade events and filters out camera jitter detected by vertical displacement
@@ -7,8 +7,8 @@ function [temporalSaccades, nasalSaccades, tsdH, tsdV, diffH, diffV, filteredH, 
 SSN = HD_GetSSN;
 FontSize = 20;
 cfg_def = [];
-cfg_def.threshH = 10;
-cfg_def.threshL = -10;
+cfg_def.threshT = 10;
+cfg_def.threshN = -10;
 cfg_def.doPlotThresholds = 1;
 cfg_def.doPlotEverything = 0;
 cfg_def.removeArtifcat = 1;
@@ -22,15 +22,30 @@ cfg = ProcessConfig(cfg_def,cfg_in);
 [AHVtsd] = GetAHV_values(orientationtouse);
 
 %% Get timestamps for the pupil trace
-[~, videofn, ext] = fileparts(FindFiles('*VT1.nvt'));
-cfg_video = [];
-cfg_video.fn = strcat(videofn, ext);
-cfg_video.removeZeros = 0 ;
+% [~, videofn, ext] = fileparts(FindFiles('*VT1.nvt'));
+% cfg_video = [];
+% cfg_video.fn = strcat(videofn, ext);
+% cfg_video.removeZeros = 0 ;
+% 
+% pos_tsd = LoadPos(cfg_video);
+% pupiltime = pos_tsd.tvec;   % it apprears that the Nvt file is 2 frames longer than the number of frames from facemap
+% pupiltime = pupiltime - pupiltime(1);
+% pupiltime = pupiltime(2:end-1); % this is maybe a temporary fix. Need to find out why there is a 2 frames discrepancy. See how many frames in mp4
 
-pos_tsd = LoadPos(cfg_video);
-pupiltime = pos_tsd.tvec;   % it apprears that the Nvt file is 2 frames longer than the number of frames from facemap
-pupiltime = pupiltime - pupiltime(1);
-pupiltime = pupiltime(2:end-1); % this is maybe a temporary fix. Need to find out why there is a 2 frames discrepancy. See how many frames in mp4
+events_ts = LoadEvents([]);
+index = strfind(events_ts.label, 'Starting Recording');
+if index{1} == 1                                 % Start Recording should be in the first or second .label position.
+    starttime = events_ts.t{1}(1);  % subtract the very first time stamp to convert from Unix time to 'start at zero' time.
+elseif index{2} == 1
+    starttime = events_ts.t{2}(1); % for session with laser events, the start recording time moves to position 2. 
+else
+    error('could not find start time for this session') 
+end
+
+[~, b, c] = fileparts(FindFile('*VT1.smi'));
+fn = strcat(b,c);
+tvec_raw = read_smi(fn);
+pupiltime = tvec_raw - starttime;
 
 %% Get the pupil trace dervied from Facemap
 f = FindFiles('*VT1_proc.mat');
@@ -48,28 +63,28 @@ diffV = tsd(pupiltime(2:end), diff(tsdV.data)');
 tstart = diffH.tvec(1);
 tend = diffH.tvec(end);
 %% Filtering
-% The horizontal trace
-cfg_filter = [];
-cfg_filter.type = 'butter';
-cfg_filter.band = 'highpass';
-cfg_filter.f = 1;
-diffH.cfg.hdr{1}.Fs = 1 / median(diff(diffH.tvec));   % append the sampling rate
-filteredH = FilterLFP(cfg_filter, diffH);
-
-% The vertical trace
-cfg_filter = [];
-cfg_filter.type = 'butter';
-cfg_filter.band = 'highpass';
-cfg_filter.f = 1;
-diffV.cfg.hdr{1}.Fs = 1 / median(diff(diffV.tvec));   % append the sampling rate
-filteredV = FilterLFP(cfg_filter, diffV);
+% % The horizontal trace
+% cfg_filter = [];
+% cfg_filter.type = 'butter';
+% cfg_filter.band = 'highpass';
+% cfg_filter.f = 1;
+% diffH.cfg.hdr{1}.Fs = 1 / median(diff(diffH.tvec));   % append the sampling rate
+% filteredH = FilterLFP(cfg_filter, diffH);
+% 
+% % The vertical trace
+% cfg_filter = [];
+% cfg_filter.type = 'butter';
+% cfg_filter.band = 'highpass';
+% cfg_filter.f = 1;
+% diffV.cfg.hdr{1}.Fs = 1 / median(diff(diffV.tvec));   % append the sampling rate
+% filteredV = FilterLFP(cfg_filter, diffV);
 
 %% Remove Artifacts from Camera Movement (vertical displacement)
 if cfg.removeArtifcat == 1
 cfg.low_freq = 10;
 cfg.high_freq = 15;
 eeg1= diffV.data;
-samp_freq = diffH.cfg.hdr{1}.Fs;
+samp_freq =  1 / median(diff(diffV.tvec));
 order = round(samp_freq); %determines the order of the filter used
 if mod(order,2)~= 0
     order = order-1;
@@ -80,11 +95,12 @@ filtered1 = Filter0(MyFilt,eeg1); %filters eeg1 between low_freq and high_freq
 filt_hilb1 = hilbert(filtered1); %calculates the Hilbert transform of eeg1
 amp1 = abs(filt_hilb1);%calculates the instantaneous amplitude of eeg1 filtered between low_freq and high_freq
 amp1=amp1-mean(amp1); %removes mean of the signal because the DC component of a signal does not change the correlation
+amp1tsd  = tsd(diffV.tvec, amp1); 
 artifactIndex = amp1 > cfg.artifactThresh; 
 suspectPoints = find(artifactIndex);
 
 %% Thresholding the TEMPORAL saccades (positive AHV segments)
-tP = diffH.data > cfg.threshH;  % data points above threshold
+tP = diffH.data > cfg.threshT;  % data points above threshold
 [~, index_tP] = find(tP);  % tvec indices for data points above threshold
 diff_tP = horzcat([NaN diff(index_tP)]);  % spacing for thresholded data
 adjacent_tP = diff_tP ==1;       % which values of index_tP are adjacent (one timestep apart)
@@ -95,7 +111,7 @@ index_tP_touse = setdiff(index_tP_temp, val_discard_tP);
 disp(strcat('Num nasal saccades = ', num2str(length(index_tP_touse))));
 
 %% Thresholding the NASAL saccades (negative AHV segments)    
-nP = diffH.data < cfg.threshL;  % data points above threshold
+nP = diffH.data < cfg.threshN;  % data points above threshold
 [~, index_nP] = find(nP);  % tvec indices for data points above threshold
 diff_nP = horzcat([NaN diff(index_nP)]);  % spacing for thresholded data
 adjacent_nP = diff_nP ==1;       % which values of index_tP are adjacent (one timestep apart)
@@ -104,12 +120,6 @@ nasalSaccades = diffH.tvec(index_nP_temp);
 [val_discard_nP, ~] = intersect(index_nP_temp, suspectPoints);
 index_nP_touse = setdiff(index_nP_temp, val_discard_nP);
 disp(strcat('Num nasal saccades = ', num2str(length(index_nP_touse))));
-
-
-
-
-
-
 
 
 %% Plot the data in a subplot
@@ -138,12 +148,15 @@ if cfg_def.doPlotThresholds == 1
     hold on
     plot(diffH.tvec, diffH.data)
     plot(diffV.tvec, diffV.data, 'm')
+    plot(amp1tsd.tvec, amp1tsd.data, 'k', 'MarkerSize', 25)
     hold on
     xlabel('Time (sec)', 'FontSize', FontSize)
     ylabel('diff pupil pos', 'FontSize', FontSize)
     title(SSN)
-    line([tstart tend], [cfg.threshH cfg.threshH], 'Color', 'k')
-    line([tstart tend], [cfg.threshL cfg.threshL], 'Color', 'k')
+    line([tstart tend], [cfg_def.threshT cfg_def.threshT], 'Color', 'r')
+    line([tstart tend], [cfg_def.threshN cfg_def.threshN], 'Color', 'g')
+    line([tstart tend], [cfg.artifactThresh cfg.artifactThresh], 'Color', 'k', 'LineStyle', '--')
+    line([tstart tend], [-cfg.artifactThresh -cfg.artifactThresh], 'Color', 'k', 'LineStyle', '--')
     plot(diffH.tvec(index_tP_touse), diffH.data(index_tP_touse), 'r.', 'MarkerSize', 25)
     plot(diffH.tvec(index_nP_touse), diffH.data(index_nP_touse), 'g.', 'MarkerSize', 25)
     set(gca, 'FontSize', FontSize)
