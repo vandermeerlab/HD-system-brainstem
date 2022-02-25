d743c0909a1cc9f5c80923252c7ea35be94e0678
stottbot <stott.jeffrey@gmail.com>
2022-02-25 14:29:58 -0500

    added couple more checks to getQEupdown()

--- quadrature/getQEupdown.m
+++ quadrature/getQEupdown.m
@@ -26,13 +26,31 @@ if nargin ~=2
 end
 
 assert(length(chA.tvec) == length(chB.tvec))   % size of the tsd's should be identical
+% Error testing: NaNs
+if sum(isnan(chA.data))>0; warning('NaNs detected in chA. Shouldnt be any NaNs'); end
+if sum(isnan(chB.data))>0; warning('NaNs detected in chB. Shouldnt be any NaNs'); end
 
-meanA = mean(chA.data); 
+meanA = mean(chA.data);   % find the mean. Up states should be well above the mean. Down states should be well below the mean. 
 meanB = mean(chB.data); 
+
+% Error testing: Values equal to the mean
+numA = sum(chA.data==meanA); if numA > 0; warning('one or more values in chA exactly equal to the mean voltage'); end
+numB = sum(chB.data==meanB); if numB > 0; warning('one or more values in chA exactly equal to the mean voltage'); end
 
 vectorA = chA.data > meanA;    % logical with value of 1's for putative 'up' states and 0's for putative 'down' states
 vectorB = chB.data > meanB;
 
 updownData = vertcat(vectorA, vectorB); 
 updownTSD = tsd(chA.tvec, updownData);
+
+
+
+
+
+
+
+
+
+
+
 
