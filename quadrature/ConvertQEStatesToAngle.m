25aeec214cf5fdeae544a5bbf5df6032e443d84c
stottbot <stott.jeffrey@gmail.com>
2022-03-03 13:36:34 -0500

    just added a conversion factor for phi to # of wheel turns

--- quadrature/ConvertQEStatesToAngle.m
+++ quadrature/ConvertQEStatesToAngle.m
@@ -1,8 +1,11 @@
-function angle_tsd = ConvertQEStatesToAngle(cfg_in, state_tsd)
+function [angle_tsd, wheel_tsd] = ConvertQEStatesToAngle(cfg_in, state_tsd)
+
 
 cfg_def = [];
 cfg_def.dphi = 1; % encoder angle step -- property of the encoder (should be obtained by calibration procedure)
-  
+cfg_def.angle_to_turn  = 1024; % this is the conversion factor for coverting integer "angle" values into # of wheel turns. This was determined empirically with a recording session on 3/3/2022. 
+
+
 % state definitions:
 % high-high: 1
 % high-low: 2
@@ -20,12 +23,16 @@ angle_tsd = state_tsd;
 angle_tsd.data = NaN*angle_tsd.data;
 angle_tsd.data(1) = 0; % assume we start at 0
 
+
 % loop over state inputs; look up step given transition between current
 % state and previous state at each step
 for iState = 2:length(state_tsd.data)
    
     angle_tsd.data(iState) = cfg.tt(state_tsd.data(iState-1), state_tsd.data(iState));
     
-end
+end    % units of "angle" here are actual integer values that represent identical forward or backward increments of the wheel (i.e. not an actual angle value)
+
 
 angle_tsd.data = cumsum(angle_tsd.data);
+wheel_tsd.tvec = angle_tsd.tvec;  
+wheel_tsd.data = angle_tsd.data./cfg.angle_to_turn; 
